aa2a934ca071a4a34f3bb25ee8ee7b5a90755e48
djoverbeatz29 <jroverby92@gmail.com>
2020-03-03 14:46:00 -0600

    Added 'Back' logic to find_user

--- app/find_user.rb
+++ app/find_user.rb
@@ -1,25 +1,34 @@
 def find_user(user)
-    
-    prompt = TTY::Prompt.new
-    while username = prompt.ask("Please enter the name of the user you're seeking: ")
-        myuser = User.find_by(name: username)
-        if myuser
-            if myuser.trips.length > 0
-                print "Here are #{username}'s trips:\n\n"
 
-                myuser.trips.each { |tr|
-                    puts "Trip ID: #{tr.id}"
-                    puts "Destination: #{tr.destination.name}"
-                    puts "Duration: #{tr.depart_date.to_s.split(' ')[0]} - #{tr.return_date.to_s.split(' ')[0]}"
-                    puts
-                }
-            else
-                puts "#{username} has no trips... yet!"
-            end
-            break
+    prompt = TTY::Prompt.new
+    while username = prompt.ask("Please enter the name of the user you're seeking, or enter 'exit' to leave: ")
+        if username == "exit"
+            return    
         else
-            puts "We couldn't find this user in our database. Please try again."
+            myuser = User.find_by(name: username)
+            if myuser
+                if myuser.trips.length > 0
+                    print "Here are #{username}'s trips:\n\n"
+
+                    myuser.trips.each { |tr|
+                        puts "Trip ID: #{tr.id}"
+                        puts "Destination: #{tr.destination.name}"
+                        puts "Duration: #{tr.depart_date.to_s.split(' ')[0]} - #{tr.return_date.to_s.split(' ')[0]}"
+                        puts
+                    }
+                else
+                    puts "#{username} has no trips... yet!"
+                end
+                break
+            else
+                puts "We couldn't find this user in our database. Please try again."
+            end
         end
     end
-
+    resp = prompt.select("Options", ["Back", "Exit"])
+    if resp == "Back"
+        find_user(user)
+    else
+        return
+    end
 end
