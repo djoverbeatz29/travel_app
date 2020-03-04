67146a3e90818c62316b2699f521e2e25737e5d5
djoverbeatz29 <jroverby92@gmail.com>
2020-03-04 13:29:00 -0600

    Added code to enable editing of user reviews

--- app/my_reviews.rb
+++ app/my_reviews.rb
@@ -1,12 +1,39 @@
 def my_reviews(user)
+    prompt = TTY::Prompt.new
     if user.reviews.length > 0
-        print "Here are #{user.name}'s reviews:\n\n"
-        user.reviews.each { |rev| print "Site: #{rev.site.name}\nLocation: #{rev.site.destination.name}\nRating: #{rev.rating} - #{rev.content }\n\n" }
+        resp = prompt.select("Here are #{user.name}'s reviews:", user.reviews.map{ |rev| rev.site.name } + ["Back"])
+        if resp == "Back"
+            return
+        else
+            rev = user.reviews.find {|review| review.site.name == resp }
+            puts "Site: #{rev.site.name}\nLocation: #{rev.site.destination.name}\nRating: #{rev.rating} - #{rev.content }\n\n"
+        end
+        resp = prompt.select("Options", ["Delete", "Edit", "Back"])
+        case resp
+        when "Back"
+            return
+        when "Delete"
+            Review.destroy(rev.id)
+        when "Edit"
+            resp = prompt.select("Choose what to edit", ["Rating", "Content", "Back"])
+            case resp
+            when "Rating"
+                rev.update(rating: prompt.slider("Choose new rating", min: 0, max: 10))
+            when "Content"
+                new_review = prompt.ask("Rewrite your review")
+                resp = prompt.select("Are you sure?", ["Yes", "No"])
+                if resp == "Yes"
+                    rev.update(content: new_review)
+                else
+                    my_reviews(user)
+                end
+            when "Back"
+                my_reviews(user)
+            end
+        end
+         
     else
         puts "#{user.name} has no reviews... yet!"
     end
-    resp = TTY::Prompt.new.select("Options", ["Exit"])
-    if resp
-        return
-    end
+
 end
