88fdcd62040f7b17319edc75b3bf72347f2871f3
mpottebaum <mpottebaum@gmail.com>
2020-03-04 14:43:25 -0600

    added system clear to my reviews

--- app/my_reviews.rb
+++ app/my_reviews.rb
@@ -1,4 +1,5 @@
 def my_reviews(user)
+    system "clear"
     prompt = TTY::Prompt.new
     if user.reviews.length > 0
         resp = prompt.select("Here are #{user.name}'s reviews:", user.reviews.map{ |rev| rev.site.name } + ["Back"])
@@ -8,30 +9,6 @@ def my_reviews(user)
         rev = user.reviews.find {|review| review.site.name == resp }
         review_menu(rev, user)
         my_reviews(user)
-        # resp = prompt.select("Options", ["Delete", "Edit", "Back"])
-        # case resp
-        # when "Back"
-        #     return
-        # when "Delete"
-        #     Review.destroy(rev.id)
-        # when "Edit"
-        #     resp = prompt.select("Choose what to edit", ["Rating", "Content", "Back"])
-        #     case resp
-        #     when "Rating"
-        #         rev.update(rating: prompt.slider("Choose new rating", min: 0, max: 10))
-        #     when "Content"
-        #         new_review = prompt.ask("Rewrite your review")
-        #         resp = prompt.select("Are you sure?", ["Yes", "No"])
-        #         if resp == "Yes"
-        #             rev.update(content: new_review)
-        #         else
-        #             my_reviews(user)
-        #         end
-        #     when "Back"
-        #         my_reviews(user)
-        #     end
-        # end
-         
     else
         prompt.select("#{user.name} has no reviews... yet!", ["Back"])
     end
