0b4e21d8a90340af3a5a11e3b866cb225bb01356
djoverbeatz29 <jroverby92@gmail.com>
2020-03-06 11:46:13 -0600

    Increased visible menu size; changed email format to username@travelapp.com

--- app/add_trip.rb
+++ app/add_trip.rb
@@ -8,7 +8,7 @@ def add_trip(user)
         system "clear"
         while resp = prompt.ask("Enter the name of the city you wish to visit, or type 'cancel' to go back: ")
             if resp == 'cancel'
-                break
+                return
             else
                 matches = Destination.where(name: resp)
                 if matches.length == 0
@@ -17,7 +17,7 @@ def add_trip(user)
                     destination = matches[0]
                     break
                 else
-                    resp = prompt.select("We found multiple matches for #{resp}. Select from the following list:", matches.map { |m| ["#{m.name}, #{m.subcountry}, #{m.country}", m.id] }.to_h)
+                    resp = prompt.select("We found multiple matches for #{resp}. Select from the following list:", matches.map { |m| ["#{m.name}, #{m.subcountry}, #{m.country}", m.id] }.to_h, per_page: 7)
                     destination = Destination.find(resp)
                     break
                 end
@@ -27,11 +27,11 @@ def add_trip(user)
     elsif resp == "By Country"
         system "clear"
         nations = Destination.all.map { |d| d.country }.uniq
-        char = prompt.select("Please select the first letter of the country you seek.", (65...65+26).map { |i| i.chr })
+        char = prompt.select("Please select the first letter of the country you seek.", (65...65+26).map { |i| i.chr }, per_page: 10)
         system "clear"
-        nat = prompt.select("Choose your country.", nations.find_all { |n| n[0] == char })
-        sub = prompt.select("Now choose your subcountry.", Destination.where(country: nat).map{ |c| c.subcountry }.uniq)
-        city = prompt.select("Finally, choose your city.", Destination.where(country: nat, subcountry: sub).map{ |c| c.name }.uniq)
+        nat = prompt.select("Choose your country.", nations.find_all { |n| n[0] == char }, per_page: 10)
+        sub = prompt.select("Now choose your subcountry.", Destination.where(country: nat).map{ |c| c.subcountry }.uniq, per_page: 10)
+        city = prompt.select("Finally, choose your city.", Destination.where(country: nat, subcountry: sub).map{ |c| c.name }.uniq, per_page: 10)
         destination = Destination.find_by(name: city, subcountry: sub, country: nat)
     else
         return
@@ -51,7 +51,7 @@ def add_trip(user)
 
     user.add_trip(destination, depart_date, return_date)
     puts "Great! You'll be heading to #{destination.name} on #{depart_date.strftime("%F")} and returning on #{return_date.strftime("%F")}."
-    puts "Your confirmation email should be arriving shortly at #{user.name.downcase.gsub(' ', '')}#{user.id}@gmail.com.\n"
+    puts "Your confirmation email should be arriving shortly at #{user.username}@travelapp.com.\n"
     resp = prompt.select("Options", ["Add another trip", "Exit"])
     if resp == "Add another trip"
         add_trip(user)
